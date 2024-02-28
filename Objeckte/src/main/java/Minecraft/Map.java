ecbae4815dfccb8fbfc60d6d118892587f06dec0
lolcklol2 <97003738+lolcklol2@users.noreply.github.com>
2024-02-28 09:32:21 +0100

    Add files via upload

---
+++ Objeckte/src/main/java/Minecraft/Map.java
@@ -0,0 +1,15 @@
+package Minecraft;
+
+public class Map {
+    long seed;
+    int x;
+    int y;
+    int[][] Map = new int[10][10];
+    public Map(long seed){
+        this.seed = seed;
+    }
+    public void Createmap(){
+
+    }
+    // TODO Build it
+}
