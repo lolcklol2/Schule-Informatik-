671669e112ec6f63b991ad67d80af742aa02b608
lolcklol2 <97003738+lolcklol2@users.noreply.github.com>
2025-03-12 23:35:24 +0100

    Delete chapter-17 directory

--- chapter-17/src/main/java/org/lwjglb/engine/IGuiInstance.java
+++
@@ -1,9 +0,0 @@
-package org.lwjglb.engine;
-
-import org.lwjglb.engine.scene.Scene;
-
-public interface IGuiInstance {
-    void drawGui();
-
-    boolean handleGuiInput(Scene scene, Window window);
-}
